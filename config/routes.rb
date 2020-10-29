edbfb5fcff439d8b3b3af49b8403a8c51139af31
wilsonng21 <wilsongch92@gmail.com>
2020-10-29 16:25:44 -0400

    able to render json format animal shelters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :donations
-  resources :comments
-  resources :animal_shelters
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/animal_shelters', to: 'animal_shelters#index'
+  
 end
