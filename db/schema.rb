e5a5a242b6013c9c0c1aa96565302724aa3f045d
wilsonng21 <wilsongch92@gmail.com>
2020-11-02 14:27:27 -0500

    able to create customer on Stripe API end and attach their customer id to rails api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_185842) do
+ActiveRecord::Schema.define(version: 2020_11_02_191212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_185842) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "customer_id"
   end
 
   add_foreign_key "comments", "animal_shelters"
