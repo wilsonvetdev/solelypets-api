d199980d09699d5d73bcb679aab63de3b880c830
wilsonng21 <wilsongch92@gmail.com>
2020-11-02 23:40:51 -0500

    user instance methods adjustment

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@ class User < ApplicationRecord
     def paid_donations_count
         Stripe.api_key = ENV['STRIPE_SECRET_KEY']
         customer = Stripe::Customer.retrieve(self.customer_id)
-        Stripe::Checkout::Session.list.find_all { |session| session.customer === self.customer_id && session.payment_status === 'paid'}.count
+        Stripe::Checkout::Session.list.find_all { |session| session.customer === customer.id && session.payment_status === 'paid'}.count
     end
 
     def total_donations_amount
         Stripe.api_key = ENV['STRIPE_SECRET_KEY']
         customer = Stripe::Customer.retrieve(self.customer_id)
-        sessions = Stripe::Checkout::Session.list.find_all { |session| session.customer === self.customer_id && session.payment_status === 'paid'}
+        sessions = Stripe::Checkout::Session.list.find_all { |session| session.customer === customer.id && session.payment_status === 'paid'}
         array_of_payments = sessions.pluck(:amount_total)
         array_of_payments.inject(0){ |sum, payment| sum + payment} / 100
     end
@@ -25,12 +25,14 @@ class User < ApplicationRecord
     def donated_to
         Stripe.api_key = ENV['STRIPE_SECRET_KEY']
         customer = Stripe::Customer.retrieve(self.customer_id)
-        sessions = Stripe::Checkout::Session.list.find_all { |session| session.customer === self.customer_id && session.payment_status === 'paid'}
+        sessions = Stripe::Checkout::Session.list.find_all { |session| session.customer === customer.id && session.payment_status === 'paid'}
+        
         array_of_unique_shelter_ids = sessions.filter { |session| session.client_reference_id }.pluck(:client_reference_id).uniq
         array_of_unique_shelter_ids = array_of_unique_shelter_ids.map { |id| id.to_i }
-        shelters = []
-        array_of_unique_shelter_ids.each { |id| shelters = AnimalShelter.all.find_all { |shelter| shelter.id === id } }
-        shelters
+        active_record_relations = []
+
+        array_of_unique_shelter_ids.each { |id| active_record_relations << AnimalShelter.where(id: id) }
+        active_record_relations.map{ |shelter| shelter[0].name }
     end
 
 
